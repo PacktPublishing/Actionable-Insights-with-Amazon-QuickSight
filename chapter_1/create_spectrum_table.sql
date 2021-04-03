b38304d0a5179655b83a922f5b41143d4841398f
Manos Samatas <manos@Manos-MacBook-Pro.local>
2021-04-03 19:33:33 +0100

    Removed old files

--- chapter_1/create_spectrum_table.sql
+++
@@ -1,5 +0,0 @@
-create external schema spectrum_schema from data catalog 
-database 'my-data-lake-db' 
-iam_role 'arn:aws:iam::xxxxxxxxxxxx:role/RedshiftSpectrumRole'
-create external database if not exists;
-
